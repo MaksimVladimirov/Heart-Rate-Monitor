cc895e125bb77086402c66a949554ccd477e1774
Maksim Vladimirov <vl4dimirov@gmail.com>
2022-12-06 16:50:08 +0300

    add backend

--- src/js/script.js
+++ src/js/script.js
@@ -47,4 +47,80 @@ $(document).ready(function () {
   }
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
+  //Modal- windows
+
+  $("[data-modal = consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("fast");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #thanks, #order").fadeOut("fast");
+  });
+  $(".button_mini").on("click", function () {
+    $(".overlay, #order").fadeIn("fast");
+  });
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__description").text(
+        $(".catalog-item__subtitle").eq(i).text()
+      );
+      $(".overlay, #order").fadeIn("fast");
+    });
+  });
+
+  //Validate form
+
+  function validateForm(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите более {0}х символов!"),
+        },
+        phone: "Пожалуйста, введите свой телефон",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты",
+        },
+      },
+    });
+  }
+  validateForm("#consultation-form");
+  validateForm("#consultation form");
+  validateForm("#order form");
+
+  $("input[name=phone]").mask("+7(999) 999-99-99");
+
+  $("form").submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function() {
+      $(this).find("input").val("");
+
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
 });
